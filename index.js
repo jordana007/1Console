d01559b839a1126596079d57aeb2b4d0fedd9099
jordana <133382207+jordana007@users.noreply.github.com>
2024-08-28 10:51:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const primeiroNome = "Jordana";
+const sobrenome = "Cardoso";
+const idade = 17;
+const altura = 1.53;
+const massa = 52;
+
+
+
+console.log(primeiroNome);
+console.log(sobrenome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+
+const imc = massa / (altura * altura);
+console.log("Imc: " imc);
+
+
+
+
+// NaN - Not a Number
